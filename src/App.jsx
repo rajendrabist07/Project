3ccdc10546da11f7cb00acecc77b62f456370562
rajendra bist <rajendrabist396@gmail.com>
2026-02-13 18:47:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Advertisement from "./Components/Advertisement"
+import Header from "./Components/Header"
+import Hero from "./Components/Hero"
+import Brand from "./Components/Brand"
+import Service from "./Components/Service"
+import Product from "./Components/Product"
+  
+function App() {
+
+
+  return (
+    <>
+      <Advertisement />
+      <Header />
+      <Hero />
+      <Brand />
+      <Service /> 
+      <Product />
+    </>
+  )
+}
+
+export default App
